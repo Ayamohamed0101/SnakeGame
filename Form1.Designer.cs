e4e60998f0177b8f228b4f2ac63dfa2be392a00a
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-08-01 22:57:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(41, 13);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score :";
+            // 
+            // lblFood
+            // 
+            this.lblFood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblFood.Image = global::Snake_Game.Properties.Resources.Legend_M54;
+            this.lblFood.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lblFood.Location = new System.Drawing.Point(201, 289);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(21, 21);
+            this.lblFood.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = global::Snake_Game.Properties.Resources.Screenshot_2024_07_31_020302;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(964, 441);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblScore);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblFood;
+    }
+}
+
---
+++ Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // important to extract label class for location,size,backcolor
+namespace Snake_Game
+{
+    internal class Piece:PictureBox // snake >>>>>>>>>>>>>>>>
+    {
+
+        public Piece(int x, int y)
+        {
+            Location = new Point(x, y);
+            Size = new Size(21, 21);
+            BackColor = Color.Red;
+            Enabled = false;
+            Image = Properties.Resources.Head;
+            BackgroundImageLayout = ImageLayout.Tile;
+        }
+    }
+}
